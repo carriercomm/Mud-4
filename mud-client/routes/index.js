b5e5e0fcce8595ef4b68e0a32b9b1324a30777fa
Sergio Schezar <sschezar@gmail.com>
2015-04-16 00:22:29 -0300

    profile routes

--- mud-client/routes/index.js
+++ mud-client/routes/index.js
@@ -1,7 +1,6 @@
 var login = require('./login');
 var signup = require('./signup');
-var mongoose = require('mongoose');
-var User = mongoose.model('User')
+var profile = require('./profile');
 
 module.exports = function(app, passport) {
 
@@ -16,31 +15,13 @@ module.exports = function(app, passport) {
     res.redirect('/');
   });
 
-  app.get('/profile', ensureAuthentication, function(req, res, next) {
-    if (req.session && req.session.passport) {
-      User.findById(req.session.passport.user, function(err, user) {
-        res.redirect('/profile/' + user._id);
-      });
-    } else {
-      res.redirect('/');
-    }
-  });
-
-  app.get('/profile/:id', ensureAuthentication, function(req, res) {
-    User.findById(req.params.id, function(err, user) {
-      res.render('profile', {
-        user: user,
-        isLoggedIn: req.isAuthenticated()
-      });
-    });
-  });
-
   app.get('/play', ensureAuthentication, function(req, res) {
     res.render('mud');
   });
 
   login(app, passport);
   signup(app, passport);
+  profile(app);
 
   function ensureAuthentication(req, res, next) {
     if (req.isAuthenticated()) {
---
+++ mud-client/routes/profile.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+module.exports = function(app) {
+  app.get('/profile', ensureAuthentication, function(req, res, next) {
+    if (req.session && req.session.passport) {
+      User.findById(req.session.passport.user, function(err, user) {
+        res.redirect('/profile/' + user._id);
+      });
+    } else {
+      res.redirect('/');
+    }
+  });
+
+  app.get('/profile/:id', ensureAuthentication, function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+      res.render('profile', {
+        user: user,
+        isLoggedIn: req.isAuthenticated()
+      });
+    });
+  });
+
+  function ensureAuthentication (req, res, next) {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      res.render('login');
+    }
+  }
+};
