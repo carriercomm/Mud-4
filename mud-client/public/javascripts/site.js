237bbcab950f2708f7191b0a6dd4befd78d8ef7e
Sergio Schezar <sschezar@gmail.com>
2015-06-09 21:28:09 -0300

    home page changes

--- mud-client/public/javascripts/site.js
+++ mud-client/public/javascripts/site.js
@@ -1,8 +1,11 @@
+var $ = window.$
+
 $('document').ready(function () {
-	$('.logo-2').addClass('animated bounceInLeft')
-	
-	setTimeout(function () {
-		$('.logo-text').removeClass('hidden')
-		$('.logo-text').addClass('animated bounceInRight')
-	}, 1000)
+  $('.logo-2').removeClass('hidden')
+  $('.logo-2').addClass('animated bounceInLeft')
+
+  setTimeout(function () {
+    $('.logo-text').removeClass('hidden')
+    $('.logo-text').addClass('animated bounceInRight')
+  }, 1000)
 })
