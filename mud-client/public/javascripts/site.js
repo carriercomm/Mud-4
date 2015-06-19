1531cb6abb9d8521eef7b082386e79f1864df19f
Sergio Schezar <sschezar@gmail.com>
2015-06-19 16:49:30 -0300

    added class weapons for player profile page

--- mud-client/public/javascripts/site.js
+++ mud-client/public/javascripts/site.js
@@ -1,12 +1,4 @@
 var $ = window.$
 
 $(document).ready(function () {
-  // menu events
-  $('#play-link').click(function () {
-    window.location.replace('http://localhost:3000/play')
-  })
-
-  $('#game-guide-link').click(function () {
-    window.location.replace('http://localhost:3000/gameguide')
-  })
 })
